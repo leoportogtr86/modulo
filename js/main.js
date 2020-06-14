728492bbbcbf6536e97fba482ebe2a19d1a6bc1e
Leo Porto <leonardoreisporto@gmail.com>
2020-06-14 08:17:17 -0300

    small update

--- js/main.js
+++ js/main.js
@@ -1,30 +1,17 @@
 let machine = document.getElementById('machine')
 let num = document.getElementById('num')
 let resultado = document.getElementById('resultado')
+let audio = document.getElementById('audio')
 
-var sampler = new Tone.Sampler({
-    "C3": "../assets/audio/som1.mp3"
-}, function () {
+machine.onclick = function () {
 
-   
+    console.log(Math.abs(Number(num.value)))
+    audio.play()
 
-
-
-    machine.onclick = function () {
-
-        console.log(Math.abs(Number(num.value)))
-
-        resultado.innerHTML = Math.abs(Number(num.value))
-        sampler.triggerAttack("C3")
-
-
-
-
-    }
-    //sampler will repitch the closest sample
+    resultado.innerHTML = Math.abs(Number(num.value))
     
-}).toDestination()
 
 
 
 
+}
